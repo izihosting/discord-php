61267843e006bb3f2a837b61af9fe18d048bb7e2
Seriously Not Beluga <79474077+OskiBoski483@users.noreply.github.com>
2022-03-17 19:33:39 +0100

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,4 @@
+<?php
+echo "GRATULACJE! - Twój bot działa prawidłowo\n";
+echo "Zobacz wiki, żeby napisać własnego bota, lub skorzystać z gotowców\n";
+echo "0.8.0 by Seriously Not Beluga#0082 (oskarniziol@gmail.com)";
